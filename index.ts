c9f67abfa884c0a2dd0ae0d16d068ddfb9ba1780
Misael Abanto <josemisaelabanto@gmail.com>
2022-08-07 02:50:57 -0500

    working listing prefixes

--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
 
-export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'hello world',
-        }),
-    };
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+export const handler = async (
+	event: APIGatewayEvent & { rawQueryString: string },
+	context: Context
+): Promise<APIGatewayProxyResult> => {
+  const query = new URLSearchParams(event.rawQueryString);
+  const prefix = query.get('prefix') || undefined;
+  console.log(prefix);
+	const s3 = new S3();
+	try {
+		const result = await s3.listObjects({
+      Bucket: BUCKET_NAME!,
+      Prefix: prefix,
+      Delimiter: '/',
+    }).promise();
+		console.log('COMMON PREFIXES', result.CommonPrefixes);
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'hello world',
+			}),
+		};
+	} catch (err) {
+		console.error(err);
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'Fatal error:' + (err as Error).message,
+			}),
+		};
+	}
 };
